4fde9c63f6d47f374b6f52688778ecb8a0292fc6
Vishal Amin <vishalamin200@gmail.com>
2026-02-21 00:35:14 +0530

    added api for fetching course details

---
+++ src/app/user/course/detail/[id]/route.ts
@@ -0,0 +1,39 @@
+import { errorResponse, successResponse } from "@/lib/apiResponse";
+import connectDB from "@/lib/db";
+
+import courseModel from "@/models/course.model";
+
+import mongoose from "mongoose";
+import { NextRequest } from "next/server";
+
+
+export const GET = async (_req: NextRequest, {params}:{params:{id:string}}) => {
+        await connectDB();
+
+        try {
+            const { id: courseId } = await params;
+
+            if (!mongoose.isValidObjectId(courseId)) {
+                return errorResponse(400, `Invalid Course Id : ${courseId}`, );
+            }
+
+            const course = await courseModel.findById(courseId);
+
+            if (!course) {
+                return errorResponse(400, "No Course Exist for Provided CourseId");
+            }
+
+            return successResponse(
+                200,
+                "Course Fetch Successfully",
+                course
+            );
+
+        } catch (error) {
+            return errorResponse(
+                400,
+                "Error In Fetching Course",
+                error instanceof Error ? error.message : "Unknown Error"
+            );
+        }
+    };
